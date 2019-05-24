497038890035365bc5761e4c0addbe4a65c86333
shashwat1998 <shashwat.pandey.1901@gmail.com>
2019-05-24 19:54:02 +0530

    Add gallery to display images in database Update upload html ui
    Gallery is created to display images with the identified names
    gives us ui to see all the images
    Gallery needs to updated so that we can retrieve and display identified object name

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+import os
+from io import BytesIO
+
+from flask import Flask, render_template, request, send_from_directory, send_file
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -21,13 +24,26 @@ def index():
 def upload_image_method():
     for upload_file in request.files.getlist("file"):
         filename = upload_file.filename
-        # destination = "".join([target,filename])
-        # upload_file.save(destination)
         binary_data = upload_file.read()
         new_file = FileContents(filename=filename, image_data=binary_data)
         db.session.add(new_file)
     db.session.commit()
     return "success"
+
+
+@app.route('/gallery')
+def get_gallery():
+    file_data = FileContents.query.all()
+    image_list = os.listdir('./images')
+    return render_template('gallery.html', table_list=file_data)
+
+
+@app.route('/upload/<id>')
+def send_image(id):
+    print("hello image")
+    file_data = FileContents.query.filter_by(id=id).first()
+    print(type(file_data))
+    return send_file(BytesIO(file_data.image_data), attachment_filename='cat.jpeg', mimetype='image/jpeg')
 
 
 if __name__ == '__main__':
