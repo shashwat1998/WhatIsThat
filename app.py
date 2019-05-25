85407edeca7a72964aefe46a2b1b32a6297cd35a
shashwat1998 <shashwat.pandey.1901@gmail.com>
2019-05-25 11:41:04 +0530

    Add additional templates and Update UI
    To ensure transition between pages
    Navigation bar was added and other pages were added to improve UI

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ class FileContents(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(100), unique=True, nullable=False)
     image_data =db.Column(db.LargeBinary)
+    results = db.relationship('ObjectDetected',backref='image')
 
 
 @app.route("/")
@@ -22,30 +23,35 @@ def index():
 
 @app.route("/upload", methods=['POST'])
 def upload_image_method():
+    count = 0
     for upload_file in request.files.getlist("file"):
         filename = upload_file.filename
         binary_data = upload_file.read()
         new_file = FileContents(filename=filename, image_data=binary_data)
         db.session.add(new_file)
-    db.session.commit()
-    return "success"
+        db.session.commit()
+        count += 1
+
+    return render_template("complete.html", num=count)
 
 
 @app.route('/gallery')
 def get_gallery():
     file_data = FileContents.query.all()
-    image_list = os.listdir('./images')
     return render_template('gallery.html', table_list=file_data)
 
 
-@app.route('/upload/<id>')
-def send_image(id):
-    print("hello image")
-    file_data = FileContents.query.filter_by(id=id).first()
+@app.route('/upload/<image_id>')
+def send_image(image_id):
+    file_data = FileContents.query.filter_by(id=image_id).first()
     print(type(file_data))
-    return send_file(BytesIO(file_data.image_data), attachment_filename='cat.jpeg', mimetype='image/jpeg')
+    return send_file(BytesIO(file_data.image_data), attachment_filename=file_data.filename, mimetype='image/jpeg')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
-
