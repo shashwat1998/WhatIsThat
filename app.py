a98166ee2680ec34fb514fb311b287f53b7d5e8c
shashwat1998 <shashwat.pandey.1901@gmail.com>
2019-05-26 23:54:39 +0530

    Add database to store processed output Update gallery html to display results in a table
    To receive the processed output
    Gallery now displays the output table after every image
    Now the image needs to be processed

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ class FileContents(db.Model):
     results = db.relationship('ObjectDetected',backref='image')
 
 
+class ObjectDetected(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    object_name = db.Column(db.String(20))
+    object_probability = db.Column(db.Float)
+    image_id = db.Column(db.Integer, db.ForeignKey('file_contents.id'))
+
+
 @app.route("/")
 def index():
     return render_template("upload_image.html")
@@ -30,6 +37,15 @@ def upload_image_method():
         new_file = FileContents(filename=filename, image_data=binary_data)
         db.session.add(new_file)
         db.session.commit()
+        list_returned = [[('n02124075', 'Egyptian_cat', 0.84743756), ('n02441942', 'weasel', 0.035382476),
+                          ('n02123045', 'tabby', 0.031043062), ('n02123159', 'tiger_cat', 0.017164368),
+                          ('n02443484', 'black-footed_ferret', 0.015669418)]]
+        for i in range(5):
+            new_data = ObjectDetected(object_name=list_returned[0][i][1],
+                                      object_probability=list_returned[0][i][2],
+                                      image=new_file)
+            db.session.add(new_data)
+            db.session.commit()
         count += 1
 
     return render_template("complete.html", num=count)
@@ -47,6 +63,12 @@ def send_image(image_id):
     print(type(file_data))
     return send_file(BytesIO(file_data.image_data), attachment_filename=file_data.filename, mimetype='image/jpeg')
 
+"""
+@app.route('/data/<image_id>')
+def send_data(image_id):
+    file_data = FileContents.query.filter_by(id=image_id).first()
+    return file_data.results
+"""
 
 @app.route('/about')
 def about():
